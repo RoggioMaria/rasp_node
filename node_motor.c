0ba0807dc3e661e07ca8cf9ededba6dc37edb1d5
RoggioMaria <100286434+RoggioMaria@users.noreply.github.com>
2023-09-06 15:53:03 +0200

    node_motor.c

--- node_motor.c
+++ node_motor.c
@@ -1,3 +1,4 @@
+//Nodo per motore 
 #include <rcl/rcl.h> //accesso funzionalità di ROS 2 basilari
 #include <rcl/error_handling.h> //gestione errori applicazioni
 #include <rcl/node.h> //creazione e gestione nodi
@@ -7,30 +8,20 @@
 
 //funzione che gestisce i messaggi della seriale
 //ConstPtr è uno shared pointer
-string serial_message(const msg_ser::String::ConstPtr& msg)
-{
-  
-  if(strcmp(msg->bodym, "error")!=0)
-  {
-    return msg;
-  }
-  else
-  {
-    return "failure";
-  }
-  
-}
 
 
 int main()
 {
-  //SetUp preliminari
-  rclc_support_t support;
-  rclc_support_init(&support, argc, argv, &allocator);
+  //parte uguale
+  rcl_init(argc, argv); //comunicazione
+  rcl_node_t node = rcl_get_zero_initialized_node(); //inizzializzazione
+  rcl_subscription_t subscription = rcl_get_zero_initialized_subscription(); //variabile con valori 0
 
- //inizializzazione del nodo motre
-  rclc_node_init_default(&my_node, "motor_node", "", &support);
+  rcl_node_options_t node_options = rcl_node_get_default_options(); //configurazione di default per il nodo
+  rcl_ret_t ret = rcl_node_init(&node, "node_motor", "", &node_options); //creazione nodo "node_sensor"
   
+  
+
 }
 
 
