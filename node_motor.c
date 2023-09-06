aeb128d468cbb2eb2a63267fa287398663f1935d
RoggioMaria <100286434+RoggioMaria@users.noreply.github.com>
2023-09-06 13:40:57 +0200

    node_motor.c
    AGGIUNTA LIBRERIE

--- node_motor.c
+++ node_motor.c
@@ -1,1 +1,7 @@
+#include <rcl/rcl.h> //accesso funzionalità di ROS 2 basilari
+#include <rcl/error_handling.h> //gestione errori applicazioni
+#include <rcl/node.h> //creazione e gestione nodi
+#include <workspace_ros/message.h> //creare messaggio per motore
+
+
 
