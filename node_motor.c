e1d058b25f0b86b232f2fd03687f426f66873346
RoggioMaria <100286434+RoggioMaria@users.noreply.github.com>
2023-09-06 15:30:26 +0200

    node_motor.c

--- node_motor.c
+++ node_motor.c
@@ -4,4 +4,33 @@
 #include <workspace_ros/message_for_motor.h> //creare messaggio per motore
 
 
+//funzione che gestisce i messaggi della seriale
+//ConstPtr è uno shared pointer
+string serial_message(const msg_ser::String::ConstPtr& msg)
+{
+  
+  if(strcmp(msg->bodym, "error")!=0)
+  {
+    return msg;
+  }
+  else
+  {
+    return "failure";
+  }
+  
+}
+
+
+int main()
+{
+  //SetUp preliminari
+  rclc_support_t support;
+  rclc_support_init(&support, argc, argv, &allocator);
+
+ //inizializzazione del nodo motre
+  rclc_node_init_default(&my_node, "motor_node", "", &support);
+  
+}
+
+
 
